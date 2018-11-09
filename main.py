c8e0ee402b034a166f72a14b6a964bc3210bb7d2
Jeremy Arden <43499071+jeremyarden@users.noreply.github.com>
2018-11-09 11:23:37 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from OpenGL.GLU import *
 from OpenGL.GLUT import *
 import sys
 
+global N
+global matrix
+
 def sumbu():
     glBegin(GL_LINES)
     glVertex2f(-500.0,0.0)
@@ -16,25 +19,14 @@ def start2d():
     gluOrtho2D(-500.0,500.0,-500.0,500.0)
 
 def plotPoint2d():
+    global N
+    global matrix
+
     glClear(GL_COLOR_BUFFER_BIT)
     glColor3f(1.0,0.0,0.0)
     glPointSize(3.0)
     sumbu()
-    N = input("Masukan banyak titik : ")
-    N = int(N)
-    while (N <= 0):
-        N = input("Masukan salah. Ulangi lagi : ")
-        N = int(N)
-    matrix = [[0 for i in range(N)] for j in range(2)]
-    for k in range (N):
-        point = input("Masukan titik dalam format x,y : ")
-        x, y = point.split(",")
-        x = float(int(x))
-        y = float(int(y))
-        print(k)
-        matrix[0][k] = x
-        matrix[1][k] = y
-        print(matrix)
+    
     glBegin(GL_POLYGON)
     for l in range(N):
         glVertex2f(matrix[0][l], matrix[1][l])
@@ -43,6 +35,9 @@ def plotPoint2d():
     
 
 def main():
+    global N
+    global matrix
+    
     glutInit(sys.argv)
     glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB)
     glutInitWindowSize(500,500)
@@ -59,6 +54,22 @@ Masukan input : """)
         type = int(type)
     if type == 1:
         start2d()
+        N = input("Masukan banyak titik : ")
+        N = int(N)
+        while (N <= 0):
+            N = input("Masukan salah. Ulangi lagi : ")
+            N = int(N)
+        matrix = [[0 for i in range(N)] for j in range(2)]
+        for k in range (N):
+            point = input("Masukan titik dalam format x,y : ")
+            x, y = point.split(",")
+            x = float(int(x))
+            y = float(int(y))
+            print(k)
+            matrix[0][k] = x    
+            matrix[1][k] = y
+            print(matrix)
         glutDisplayFunc(plotPoint2d)
+
     glutMainLoop()
 main()
